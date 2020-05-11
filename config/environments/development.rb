4f252ad25d06ffd117e9d33b76aad0196f06613d
AJDP <jduplooy050@gmail.com>
2020-05-11 23:27:21 +0200

    update: when webpacker assets precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.assets.initialize_on_precompile = true
+  config.assets.compile = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
