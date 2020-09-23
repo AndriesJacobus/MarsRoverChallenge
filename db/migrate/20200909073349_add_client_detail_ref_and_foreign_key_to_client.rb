2ee0cf1b493bf63e8901d92b57f3ef561b286f24
AJDP <jduplooy050@gmail.com>
2020-09-23 09:42:51 +0200

    update: resources

--- db/migrate/20200909073349_add_client_detail_ref_and_foreign_key_to_client.rb
+++ db/migrate/20200909073349_add_client_detail_ref_and_foreign_key_to_client.rb
@@ -1,6 +1,6 @@
 class AddClientDetailRefAndForeignKeyToClient < ActiveRecord::Migration[5.1]
   def change
     add_reference :client_details, :client
-    add_foreign_key :client_details, :client
+    add_foreign_key :client_details, :clients
   end
 end
