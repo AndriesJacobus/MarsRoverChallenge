8c23ba3a83d328bfb7c88c1dc3e1fecb7dd1f570
AJDP <jduplooy050@gmail.com>
2020-10-01 11:53:30 +0200

    feat: adding redis to the production build for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://wi-i-cloud.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://wi-i-cloud.herokuapp.com', 'http://wi-i-cloud.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
