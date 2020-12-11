51e66399333a1c76d17345e70c76724415b1c9b4
AJDP <jduplooy050@gmail.com>
2020-12-11 22:51:06 +0200

    fix: client_details migration

--- db/migrate/20200909073349_add_client_detail_ref_and_foreign_key_to_client.rb
+++ db/migrate/20200909073349_add_client_detail_ref_and_foreign_key_to_client.rb
@@ -7,7 +7,5 @@ class AddClientDetailRefAndForeignKeyToClient < ActiveRecord::Migration[5.1]
   def down
     remove_reference :clients, :client_detail
     remove_foreign_key :clients, :client_details
-
-    remove_column :client_details, :client_id
   end
 end
